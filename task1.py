efb94d82c76c276d7cfd83a86a148073d2cb9895
ThanaphatJ1452 <53165482+ThanaphatJ1452@users.noreply.github.com>
2020-01-22 19:15:15 +0700

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+sentence_Int = str()
+
+def main():
+    
+    print("Input: ")
+    sentence = input()
+    convertToInt(sentence)
+
+def convertToInt(sentence):
+    global sentence_Int
+    
+    for c in sentence.lower():
+        if(ord(c) >= 97 and ord(c) <= 122):
+            sentence_Int += str(ord(c) % 96) + " "      
+    print(sentence_Int)
+            
+if __name__ == "__main__":
+    try:
+        main()
+    except:
+        print("Error")
