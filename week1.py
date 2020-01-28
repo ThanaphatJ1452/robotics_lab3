d9bf07f0e40f32c3511d9a360d34a5e3cf3298eb
ThanaphatJ1452 <53165482+ThanaphatJ1452@users.noreply.github.com>
2020-01-28 16:47:17 +0700

    Update week1.py

--- week1.py
+++ week1.py
@@ -1,16 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
 sentence_Int = str()
 
 def main():
     
     print("Input: ")
     sentence = input()
-    convertToInt(sentence)
+    print(alphabet_position(sentence))
 
 def alphabet_position(sentence):
     global sentence_Int
@@ -18,7 +12,7 @@ def alphabet_position(sentence):
     for c in sentence.lower():
         if(ord(c) >= 97 and ord(c) <= 122):
             sentence_Int += str(ord(c) % 96) + " "      
-    print(sentence_Int)
+    return sentence_Int.strip()
             
 if __name__ == "__main__":
     try:
